2ee6376b80ecec6a98417901c1acaa6ec0223b5f
dinh.tran.minh.hieu <dinh.tran.minh.hieu@sun-asterisk.com>
2021-02-08 15:01:15 +0700

    modify fbs webhook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post :cw_webhook, to: "chatbot_webhooks#cw_hook"
-  get :fbs_webhook, to: "fbs_webhooks#fbs_webhook"
-  post :fbs_webhook, to: "fbs_webhooks#fbs_webhook"
+  get :webhook, to: "fbs_webhooks#fbs_webhook"
+  post :webhook, to: "fbs_webhooks#fbs_webhook"
   get :privacy, to: "static_pages#privacy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
